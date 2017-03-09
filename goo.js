607cad66279f59dcc3416553bfa9971293c240f2
Yuxuan <ychen14@stuy.edu>
2017-03-09 14:06:44 -0500

    k

--- goo.js
+++ goo.js
@@ -5,24 +5,23 @@ var circle = function(e){
     var x = e.offsetX;
     var y = e.offsetY;
     e.stopPropagation();
-    svg.appendChild(drawCircle(x,y,e));
-    
+    c = drawCircle(x,y);
+    svg.appendChild(c);
+    c.addEventListener('click',changeColor(c));    
 };
 
-var drawCircle = function(x,y,e){
-    e.stopPropagation();
+var drawCircle = function(x,y){
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c.setAttributeNS(null,"cx",x);
     c.setAttributeNS(null,"cy",y);
     c.setAttributeNS(null,"r",10);
     c.setAttributeNS(null,"fill","black");
-    c.addEventListener('click',changeColor(c,e),false);
+
     console.log("draw new circle");
     return c;
 };
 
-var changeColor = function(c,e) {
-    e.stopPropagation();
+var changeColor = function(c) {
     c.setAttributeNS(null,"fill","grey");
     console.log("change color");
 };
