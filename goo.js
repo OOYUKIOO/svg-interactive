ec7f51ceb9c46e34d22cfff1588e1407c4b07d9a
OOYUKIOO <yuxuan.chen.up@gmail.com>
2017-03-09 05:34:37 -0500

    modify

--- goo.js
+++ goo.js
@@ -4,17 +4,27 @@ var clear = document.getElementById("clear");
 var circle = function(e){
     var x = e.offsetX;
     var y = e.offsetY;
-    svg.appendChild(drawCircle(x,y));
+    e.stopPropagation();
+    svg.appendChild(drawCircle(x,y,e));
+    
 };
 
-var drawCircle = function(x,y){
+var drawCircle = function(x,y,e){
+    e.stopPropagation();
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c.setAttributeNS(null,"cx",x);
     c.setAttributeNS(null,"cy",y);
     c.setAttributeNS(null,"r",10);
     c.setAttributeNS(null,"fill","black");
-    c.addEventListener('click',changeColor);
+    c.addEventListener('click',changeColor(c,e),false);
+    console.log("draw new circle");
     return c;
+};
+
+var changeColor = function(c,e) {
+    e.stopPropagation();
+    c.setAttributeNS(null,"fill","grey");
+    console.log("change color");
 };
 
 var Clear = function() {
@@ -25,5 +35,5 @@ var Clear = function() {
 
 
 
-svg.addEventListener('click',circle);
+svg.addEventListener('click',circle,false);
 clear.addEventListener('click',Clear);
